fea5885c3ec467e559dce68591056e8c4b7a1d0c
andrealoda <andrea@casaloda.it>
2026-03-04 17:14:16 +0100

    fix .join method in task 5 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -51,5 +51,5 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.join(', ');
+const teachersString = teachers.join(' ');
 console.log(teachersString);
