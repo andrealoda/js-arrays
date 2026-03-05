17b1fa90c798996fce86e8eae2450f78c9f51daa
andrealoda <andrea@casaloda.it>
2026-03-05 19:23:32 +0100

    fix solution for task 4 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,13 +35,17 @@ console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
-for (let i = 0; i < teachers.length; i++) {
-  if (teachers[i] === 'Ed') {
-    teachers.splice(i, 1)
-  }
-}
+// for (let i = 0; i < teachers.length; i++) {
+//   if (teachers[i] === 'Ed') {
+//     teachers.splice(i, 1)
+//   }
+// }
+
+teachers.splice(teachers.indexOf('Ed'), 1);
 
 console.log(teachers);
+
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
